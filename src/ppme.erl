2afa783656be6ef6129dae052aee48b16bf73c05
Yurii Skrynnykov <truef1s7@gmail.com>
2023-09-17 21:05:37 +0300

    Add arty image generator function

--- src/ppme.erl
+++ src/ppme.erl
@@ -1,6 +1,6 @@
 -module(ppme).
 
--export([save_to_ppm/4, test_run/4, gradient/4]).
+-export([save_to_ppm/4, test_run/4, gradient/4, arty/4]).
 
 save_to_ppm(FileName, DataList, Width, Height) when is_list(DataList) ->
   {ok, IoDevice} = file:open(FileName, [write]),
@@ -16,3 +16,7 @@ test_run(FileName, Width, Height, ColorTriplet) ->
 gradient(FileName, Width, Height, BaseColorTriplet) ->
   DataList = [lists:duplicate(Width, round(BaseColorTriplet + (S / Height) * 255)) || S <- lists:seq(0, Height)],
   save_to_ppm(FileName, lists:flatten(DataList), Width, Height).
+
+arty(FileName, Width, Height, ColorTriplet) ->
+  DataList = [round(ColorTriplet + math:cos(V) + math:sin(V / Width) * 255) || V <- lists:seq(0, Width * Height)],
+  save_to_ppm(FileName, DataList, Width, Height).
